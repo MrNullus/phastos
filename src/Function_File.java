09f8034c2efd214d6810b286d02bfef8f9cd7185
MrNullus <gustavojs417@gmail.com>
2024-09-20 14:51:17 -0300

    feat: load content files

--- src/Function_File.java
+++ src/Function_File.java
@@ -1,7 +1,10 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Function_File {
     GUI gui;
+    String fileName, fileAddress;
 
     public Function_File(GUI gui) {
         this.gui = gui;
@@ -15,5 +18,28 @@ public class Function_File {
     public void open() {
         FileDialog fd = new FileDialog(gui.window, "Open File", FileDialog.LOAD);
         fd.setVisible(true);
+
+        if (fd.getFile() != null) {
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+
+            gui.window.setTitle(fileName);
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+            gui.textArea.setText("");
+
+            String line = null;
+
+            // readline => read line by line
+            while ((line = br.readLine()) != null) {
+                gui.textArea.append(line + "\n");
+            }
+
+            br.close();
+        } catch (Exception e) {
+            System.out.println("FILE NOT OPENED!");
+        }
     }
 }
